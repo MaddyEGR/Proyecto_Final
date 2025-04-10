63abc66ccb6c033b7484536fff8983b3a880d730
Madheline Gonzalez <madheline.gonzalez@galileo.edu>
2025-04-10 00:26:19 -0600

    Modificar archivos para el backend

--- modelo/Habit.js
+++ modelo/Habit.js
@@ -33,6 +33,11 @@ const habitSchema = new mongoose.Schema({
       type: Date,
       default: Date.now
     },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
     
   });
   const Habit = mongoose.model('Habit', habitSchema, 'habits');
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,47 @@
 var express = require('express');
 var router = express.Router();
 const Habit = require ('../modelo/Habit');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    return res.status(401).json({ error: "Acceso denegado. No se proporciono el token" });
+  }
+
+  try {
+    const tokenWithoutBearer = token.replace("Bearer ", "");
+    const verified = jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET);
+    req.user = verified; //Guardamos el usuario verificado en la request
+    next();
+  }catch (error) {
+    console.error(error);
+    return res.status(403).json({ error: "Token invalido o expirado" });
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/habits', async (req, res, next) => {
+router.get('/habits',authenticateToken, async (req, res, next) => {
   try{
-    const habits = await Habit.find();
+    let userId = req.user && req.user.userId ? req.user.userId : res.status(500).json({ error: "No se pudo obtener el id del usuario" });
+    const habits = await Habit.find({'userId': new mongoose.Types.ObjectId(userId) });
     res.json(habits);  
   }catch (error){
     res.status(500).json({ message: "Error al obtener los habitos" });
   }
 });
 
-router.post('/habits', async (req, res, next) => {
+router.post('/habits',authenticateToken, async (req, res, next) => {
   try{
     const { title, description } = req.body;
-    const habit = new Habit({ title, description });
+    let userId = req.user && req.user.userId ? req.user.userId : res.status(500).json({ error: "Error al agregar el habito" });
+    userId = new mongoose.Types.ObjectId(userId);
+    const habit = new Habit({ title, description, userId });
     await habit.save();
     res.json(habit);
   }
@@ -28,7 +50,7 @@ router.post('/habits', async (req, res, next) => {
   }
 });
 
-router.delete('/habits/:id', async (req, res) => {
+router.delete('/habits/:id',authenticateToken, async (req, res) => {
   try{
     await Habit.findByIdAndDelete(req.params.id);
     res.json({ message: "Habito Eliminado" });
@@ -37,7 +59,7 @@ router.delete('/habits/:id', async (req, res) => {
   }
 }); 
 
-router.patch('/habits/markasdone/:id', async (req, res) => {
+router.patch('/habits/markasdone/:id',authenticateToken, async (req, res) => {
   try{
     const habit = await Habit.findById(req.params.id);
     habit.lastDone = new Date();
