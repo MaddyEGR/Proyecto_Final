19427b3f310bf59d867acaf56473ce3049e0bbc4
Madheline Gonzalez <madheline.gonzalez@galileo.edu>
2025-04-17 21:58:23 -0600

    Fix store export issues

--- Redux/store.ts
+++ Redux/store.ts
@@ -14,3 +14,4 @@ export const makeStore = () => {
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
+export type RootState = ReturnType<AppStore["getState"]>;
